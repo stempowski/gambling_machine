5e46886e958864d98098f532ed7a1db0d5562c49
stempowski <stempowski.m@gmail.com>
2023-05-30 14:02:14 +0200

    function deposit function get_number_of_lines

---
+++ machine_functions.py
@@ -0,0 +1,32 @@
+MAX_LINES = 3 
+def deposit():
+    while(True):
+        amount = input("how much you would like to deposit? PLN___")
+        if amount.isdigit():
+            amount = int(amount)
+            if(amount) > 0:
+                print("ok, you deposited: " + str(amount))
+                break
+            else:
+                print("please deposit more than 0")
+        else:
+            print("please enter digit number")
+
+    return amount
+
+def get_number_of_lines():
+    while(True):
+        number_of_lines = ("enter number of lines between 1 and " + str(MAX_LINES))
+        if number_of_lines.isdigit():
+            number_of_lines = int(number_of_lines)
+            if(number_of_lines > 0):
+                print("you entered: " + number_of_lines + " number of lines")
+                break
+            else:
+                print("please enter positive number of lines! ")
+    
+
+def main():
+    balance = deposit()
+
+main()
